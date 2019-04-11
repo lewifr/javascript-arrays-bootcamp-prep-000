3955c9a2e8f8bc9e68d267a7fe21aad9dac39bcf
Francis Lewis <francis.lewis@me.com>
2019-04-11 17:23:53 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,13 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray (arr, element) {
-  arr.unshift(element)
-  return arr
+  return [element, ...arr]
+
 }
 
 function destructivelyAddElementToBeginningOfArray (arr, element) {
-  return [element, ...arr]
+  arr.unshift(element)
+  return arr
 }
 
 function addElementToEndOfArray (arr, element) {
